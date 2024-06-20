032e1dc32725e955c1425fdcc1527129c25e9df2
Houcine Walaq <houcine.walaq@gmai.com>
2024-06-20 14:48:47 +0100

    fix 2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 
+
 const router = express.Router();
+router.use(express.json());
 
 router.get('/status', AppController.getStatus);
+
 router.get('/stats', AppController.getStats);
+
 
 export default router;
